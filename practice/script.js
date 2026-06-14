4c2f13a7c9be090891137b2f6c3de64f99f51eea
KlmY0ngBe0m <egg3797@gmail.com>
2026-06-14 22:39:39 +0900

    入力した予算と旅行期間の表示機能を追加

--- practice/script.js
+++ practice/script.js
@@ -1,5 +1,9 @@
 const recommendButton = document.querySelector("#recommend-button");
 
 recommendButton.addEventListener("click", function() {
-    alert("추천 버튼을 눌렀습니다!");
+   const budget = document.querySelector("#budget").value;
+   const days = document.querySelector("#days").value;
+   const result = document.querySelector("#result");
+
+   result.textContent = `예산 ${budget}원의로 ${days}일 여행을 추천합니다.`; 
 });
