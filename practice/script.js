e6751740c722d13be997ad69e03dde26f3c9b0d1
KlmY0ngBe0m <egg3797@gmail.com>
2026-06-14 22:48:32 +0900

    同行者と旅行スタイルの表示機能を追加

--- practice/script.js
+++ practice/script.js
@@ -3,7 +3,10 @@ const recommendButton = document.querySelector("#recommend-button");
 recommendButton.addEventListener("click", function() {
    const budget = document.querySelector("#budget").value;
    const days = document.querySelector("#days").value;
+   const compaion = document.querySelector("#companion").value;
+   const travelStyle = document.querySelector("#travel-style").value;
    const result = document.querySelector("#result");
 
-   result.textContent = `예산 ${budget}원의로 ${days}일 여행을 추천합니다.`; 
+   result.textContent = 
+    `예산 ${budget}원의로 ${days}일 동안 ${compaion} 유형과 ${travelStyle} 스타일에 맞는 여행을 추천합니다.`; 
 });
