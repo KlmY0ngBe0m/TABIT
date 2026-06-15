f7cab84029cc53bc5eceec40100d6ce50b908a94
KlmY0ngBe0m <egg3797@gmail.com>
2026-06-15 14:49:27 +0900

    選択した興味を表示する機能を追加

--- practice/script.js
+++ practice/script.js
@@ -3,10 +3,18 @@ const recommendButton = document.querySelector("#recommend-button");
 recommendButton.addEventListener("click", function() {
    const budget = document.querySelector("#budget").value;
    const days = document.querySelector("#days").value;
-   const compaion = document.querySelector("#companion").value;
+   const companion = document.querySelector("#companion").value;
    const travelStyle = document.querySelector("#travel-style").value;
+   const checkedInterests = document.querySelectorAll(
+    'input[name="interest"]:checked'
+   );
+
+   const interests = Array.from(checkedInterests).map(function (checkbox) {
+    return checkbox.value;
+   });
    const result = document.querySelector("#result");
 
    result.textContent = 
-    `예산 ${budget}원의로 ${days}일 동안 ${compaion} 유형과 ${travelStyle} 스타일에 맞는 여행을 추천합니다.`; 
+    `예산 ${budget}원으로 ${days}일 동안 ${companion} 유형과 ${travelStyle} 스타일에 맞는 여행을 추천합니다.
+    관심사는 ${interests.join(", ")}입니다.`; 
 });
