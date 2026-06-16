24606ae79b90e5753a64c14089c4f8a3554cae24
KlmY0ngBe0m <egg3797@gmail.com>
2026-06-16 20:45:55 +0900

    旅行先の推薦理由を追加

--- practice/script.js
+++ practice/script.js
@@ -15,13 +15,17 @@ recommendButton.addEventListener("click", function() {
    const result = document.querySelector("#result");
 
    let recommendedCity = "도쿄";
+   let recommendedReson = "처음 일본 여행을 준비하는 여행자에게 다양한 선택지가 있기 때문입니다."
 
    if (interests.includes("nature")) {
     recommendedCity = "삿포로";
+    recommendedReson = " 자연, 계절 풍경, 여유로운 여행을 즐기기에 좋기 떄문입니다.";
    } else if (interests.includes("food")) {
     recommendedCity = "후쿠오카";
+    recommendedReson = "라멘, 야타이, 해산물 등 먹거리 중심 여행에 적합하기 때문입니다.";
    } else if (interests.includes("shopping")) {
     recommendedCity = "오사카";
+    recommendedReson = "쇼핑, 맛집, 도심 관광을 함께 즐기기 좋기 때문입니다.";
    }
 
    if (budget === "") {
@@ -35,6 +39,6 @@ recommendButton.addEventListener("click", function() {
    }
 
    result.textContent = 
-    `추천 여행지는 ${recommendedCity}입니다. 예산 ${budget}원으로 ${days}일 동안 ${companion} 유형과 ${travelStyle} 스타일에 맞는 여행을 추천합니다.
+    `추천 여행지는 ${recommendedCity}입니다. 추천 이유: ${recommendedReson} 예산 ${budget}원으로 ${days}일 동안 ${companion} 유형과 ${travelStyle} 스타일에 맞는 여행을 추천합니다.
     관심사는 ${interests.join(", ")}입니다.`; 
 });
