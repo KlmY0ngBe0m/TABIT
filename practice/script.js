bf90c2c5c66eb5859553a94244122bf66a9770f6
KlmY0ngBe0m <egg3797@gmail.com>
2026-06-17 16:53:15 +0900

    プロトタイプのコード整理とREADME更新

--- practice/script.js
+++ practice/script.js
@@ -21,7 +21,7 @@ recommendButton.addEventListener("click", function() {
    const result = document.querySelector("#result");
 
    let recommendedCity = "도쿄";
-   let recommendedReson = "처음 일본 여행을 준비하는 여행자에게 다양한 선택지가 있기 때문입니다."
+   let recommendationReason = "처음 일본 여행을 준비하는 여행자에게 다양한 선택지가 있기 때문입니다.";
    let estimatedBudget = "약 80만 원";
    let samplePlan = [
      "1일차: 도쿄 도착과 시부야 관광", 
@@ -31,8 +31,8 @@ recommendButton.addEventListener("click", function() {
 
    if (interests.includes("자연")) {
     recommendedCity = "삿포로";
-    recommendedReson = " 자연, 계절 풍경, 여유로운 여행을 즐기기에 좋기 떄문입니다.";
-    estimatedBudget = "약 90만원";
+    recommendationReason = "자연, 계절 풍경, 여유로운 여행을 즐기기에 좋기 때문입니다.";
+    estimatedBudget = "약 90만 원";
     samplePlan = [
         "1일차: 삿포로 시내 관광", 
         "2일차: 오타루 당일치기",
@@ -40,17 +40,17 @@ recommendButton.addEventListener("click", function() {
     ];
    } else if (interests.includes("맛집")) {
     recommendedCity = "후쿠오카";
-    recommendedReson = "라멘, 야타이, 해산물 등 먹거리 중심 여행에 적합하기 때문입니다.";
-    estimatedBudget = "약 75만원";
+    recommendationReason = "라멘, 야타이, 해산물 등 먹거리 중심 여행에 적합하기 때문입니다.";
+    estimatedBudget = "약 75만 원";
     samplePlan = [
-    "1일차: 하카타  도착과 라멘 거리",
+    "1일차: 하카타 도착과 라멘 거리",
      "2일차: 다자이후와 야타이 체험",
      "3일차: 쇼핑"]
     ;
    } else if (interests.includes("쇼핑")) {
     recommendedCity = "오사카";
-    recommendedReson = "쇼핑, 맛집, 도심 관광을 함께 즐기기 좋기 때문입니다.";
-    estimatedBudget = "약 85만원";
+    recommendationReason = "쇼핑, 맛집, 도심 관광을 함께 즐기기 좋기 때문입니다.";
+    estimatedBudget = "약 85만 원";
     samplePlan = [
         "1일차: 난바와 도톤보리",
         "2일차: 우메다 쇼핑", 
@@ -64,7 +64,7 @@ recommendButton.addEventListener("click", function() {
    }
 
    if (Number(budget) < 30) {
-    result.textContent = "예산은 최소 30만 원 이상으로 입력해주새요.";
+    result.textContent = "예산은 최소 30만 원 이상으로 입력해주세요.";
     return;
    }
 
@@ -77,9 +77,9 @@ recommendButton.addEventListener("click", function() {
 
    result.innerHTML = 
     `추천 여행지: ${recommendedCity}<br>` +
-    `추천 이유: ${recommendedReson}<br>` +
+    `추천 이유: ${recommendationReason}<br>` +
     `예상 예산: ${estimatedBudget}<br>` +
-    `간단 일정: <br>${planByDays}<br>` +
+    `간단 일정:<br>${planByDays}<br>` +
     `예산: ${budget}만 원<br>` +
     `여행 기간: ${days}일<br>` +
     `동행 유형: ${companion}<br>` +
